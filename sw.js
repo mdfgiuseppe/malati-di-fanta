46545bc44aa428a689085180e0daffb0ea760f90
mdfgiuseppe <joexy79@gmail.com>
2026-05-09 09:14:24 +0200

    feat: PWA icon e manifest con logo Malati di Fanta (apple-touch-icon + manifest dinamico)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260509_0905';
+const CACHE_VERSION = '20260509_0914';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 // Installa subito senza aspettare che le vecchie tab si chiudano
