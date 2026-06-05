c0819cb284544da95351c0359d35d6c74cfcbcb0
mdfgiuseppe <joexy79@gmail.com>
2026-06-05 14:32:34 +0200

    debug: aggiungi log a showAdminSection per verificare quale id viene passato

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605_1429';
+const CACHE_VERSION = '20260605_1432';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
