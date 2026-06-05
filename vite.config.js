08bc60bcf5777fcf4203ca6b9fa1b849c67f1cbb
mdfgiuseppe <joexy79@gmail.com>
2026-06-05 18:27:46 +0200

    config: deploy new app to /v2/, keep old app at root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/malati-di-fanta/v2/',
   plugins: [vue()],
   server: {
     port: 5173,
