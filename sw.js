ecf6af001784aa316d025bd5657933e396f14321
mdfgiuseppe <joexy79@gmail.com>
2026-06-05 14:13:13 +0200

    fix: aumenta throttling max concurrent a 5 per permettere caricamento utenti

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605_1331';
+const CACHE_VERSION = '20260605_1413';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
