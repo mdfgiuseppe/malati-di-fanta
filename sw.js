5d2c1d490b69e3158bcf7b0d95a8ee3813b7166d
mdfgiuseppe <joexy79@gmail.com>
2026-06-05 14:25:51 +0200

    fix: carica utenti e richieste pendenti sempre (non dipende da GS_ONLINE)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605_1422';
+const CACHE_VERSION = '20260605_1425';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
