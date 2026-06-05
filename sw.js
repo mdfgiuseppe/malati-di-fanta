11c5bd2d6f546ca05dd89e960c3d5aa6961671d5
mdfgiuseppe <joexy79@gmail.com>
2026-06-05 17:09:02 +0200

    debug: vari fix e logging per admin utenti

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605_1434';
+const CACHE_VERSION = '20260605_1709';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
