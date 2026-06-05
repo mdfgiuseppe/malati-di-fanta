523c11d3499fa7f0cc7e136d6ef82fe9c6ec4452
mdfgiuseppe <joexy79@gmail.com>
2026-06-05 14:34:14 +0200

    debug: aggiungi log dentro if(id==='utenti') per verificare se entra nel blocco

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605_1432';
+const CACHE_VERSION = '20260605_1434';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
