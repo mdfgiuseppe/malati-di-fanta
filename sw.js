215429b18bf3ea1cb1a76ee5dc6c8349f028bba8
mdfgiuseppe <joexy79@gmail.com>
2026-06-05 13:10:37 +0200

    fix: aggiungi throttling per limitare richieste simultanee a Google Sheets (max 3)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605_1308';
+const CACHE_VERSION = '20260605_1310';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
