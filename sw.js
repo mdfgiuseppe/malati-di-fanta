ef630e253b2de3d606c80128eafb4e176eb8182a
mdfgiuseppe <joexy79@gmail.com>
2026-06-05 13:23:28 +0200

    fix: salva cambio squadre su GSheets dopo conferma modifiche

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605_1310';
+const CACHE_VERSION = '20260605_1323';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
