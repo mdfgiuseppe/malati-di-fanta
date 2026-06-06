86836a25ff0e8d5c7bf64389e51ad8c921714de9
mdfgiuseppe <joexy79@gmail.com>
2026-06-06 16:52:06 +0200

    perf: ottimizza caricamento GSheet - 50 richieste parallele, timeout 30s, priorità rose admin

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260606_1338';
+const CACHE_VERSION = '20260606_1652';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
