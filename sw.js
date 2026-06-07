e38b41f546cfaee6c7445c5c4ad41f6fc0075385
mdfgiuseppe <joexy79@gmail.com>
2026-06-07 17:59:13 +0200

    fix: aggiorna window._TEAMS_ROSE_IT quando salvi rose (rimane sincronizzato dopo HR)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260607_0828';
+const CACHE_VERSION = '20260607_1759';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
