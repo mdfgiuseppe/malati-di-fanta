00e742c91f32d8fb9c6a80a8cbd37fa6c941c595
mdfgiuseppe <joexy79@gmail.com>
2026-06-10 21:18:47 +0200

    debug: log completo di mData

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260610_2111';
+const CACHE_VERSION = '20260610_2118';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
