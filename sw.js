1dc6a5dae44eb60bd420522226fc10aa59dfe378
mdfgiuseppe <joexy79@gmail.com>
2026-06-10 19:51:44 +0200

    fix: toggle con focus/blur - deseleziona solo al secondo click sulla stessa competizione

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260610_1948';
+const CACHE_VERSION = '20260610_1951';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
