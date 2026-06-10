286bb96dcbfd2bd3ec5ac619c784d6b28492c697
mdfgiuseppe <joexy79@gmail.com>
2026-06-10 21:09:33 +0200

    fix: aggiungi pulsante 'Modifica classifica' anche nella sezione generica

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260610_2104';
+const CACHE_VERSION = '20260610_2109';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
