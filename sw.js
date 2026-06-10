e590ccbf9c746431b540ebf437a88d8dce99d7d1
mdfgiuseppe <joexy79@gmail.com>
2026-06-10 18:39:19 +0200

    feat: toggle coppa/qualificazione - riclicca per nascondere l'editor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260610_1825';
+const CACHE_VERSION = '20260610_1839';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
