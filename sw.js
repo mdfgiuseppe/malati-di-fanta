d0cd539f2b5f193989505fc79afca619eae29db0
mdfgiuseppe <joexy79@gmail.com>
2026-06-10 19:44:25 +0200

    fix: toggle semplice - onchange mostra subito, onmousedown deseleziona al secondo click

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260610_1929';
+const CACHE_VERSION = '20260610_1944';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
