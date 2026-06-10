8b92629ea3821f3621b61069bf1668ad5513892b
mdfgiuseppe <joexy79@gmail.com>
2026-06-10 20:52:29 +0200

    debug: aggiungi console.log al salvataggio qualificazioni

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260610_1953';
+const CACHE_VERSION = '20260610_2052';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
