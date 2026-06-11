04cd5a10652b638a5a3ada8ef3af76662d7881eb
mdfgiuseppe <joexy79@gmail.com>
2026-06-11 13:37:16 +0200

    debug: aggiungi logging a admMoveQualifRanking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260610_2126';
+const CACHE_VERSION = '20260611_1337';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
