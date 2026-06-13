26eba36b3d27d87b19f77e6cd3e4c80ea58eefdc
mdfgiuseppe <joexy79@gmail.com>
2026-06-13 23:47:48 +0200

    fix: select qualificazione coppe sempre vuoto al caricamento, elimina preselezionamento

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260613_2343';
+const CACHE_VERSION = '20260613_2347';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
