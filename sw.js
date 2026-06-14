4264881e2dd30492cfe94d77d761e847e0117681
mdfgiuseppe <joexy79@gmail.com>
2026-06-14 21:38:31 +0200

    fix: rendi regex risultato più flessibile per spazi (4-1 o 4 - 1)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260614_2137';
+const CACHE_VERSION = '20260614_2138';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
