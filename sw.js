e015bbd22487f507782488a371223115d7ef87ce
mdfgiuseppe <joexy79@gmail.com>
2026-06-14 21:52:04 +0200

    debug: aggiungi logs per tracciare il caricamento dati e il matching

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260614_2147';
+const CACHE_VERSION = '20260614_2152';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
