f8a2c4189771063407284ec580c7b63e9f210061
mdfgiuseppe <joexy79@gmail.com>
2026-06-14 09:13:25 +0200

    fix: carica ranking override da GSheet quando apri classifica e sincronizza con localStorage

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260614_0903';
+const CACHE_VERSION = '20260614_0913';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
