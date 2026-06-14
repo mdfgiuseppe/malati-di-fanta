24cefee52c4b6675b571a502ec5d865fafe19a43
mdfgiuseppe <joexy79@gmail.com>
2026-06-14 21:08:41 +0200

    fix: attendi caricamento ranking override da GSheet prima di renderizzare qualificazione lato utente

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260614_1930';
+const CACHE_VERSION = '20260614_2108';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
