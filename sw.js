64e92f303d9bac23dc2ddfb0ec38f81d033dd075
mdfgiuseppe <joexy79@gmail.com>
2026-06-14 21:37:20 +0200

    fix: correggi indici colonne import Champions (A, F, G)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260614_2134';
+const CACHE_VERSION = '20260614_2137';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
