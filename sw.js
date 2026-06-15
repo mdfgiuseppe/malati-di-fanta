39329b59fa9796c46328726c1f03f30f62c8a9b3
mdfgiuseppe <joexy79@gmail.com>
2026-06-15 21:32:39 +0200

    fix: salta partite incomplete quando cerca il match da importare

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260615_2131';
+const CACHE_VERSION = '20260615_2132';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
