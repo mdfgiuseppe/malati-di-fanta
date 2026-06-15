846165843e4afcb75442546048c3d476f471ef0b
mdfgiuseppe <joexy79@gmail.com>
2026-06-15 21:07:14 +0200

    fix: salva nome squadra completo quando passa al turno successivo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260615_2104';
+const CACHE_VERSION = '20260615_2107';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
