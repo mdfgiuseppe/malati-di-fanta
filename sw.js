0a8648f7ea74a065e478468d7cb8e604d2f1078e
mdfgiuseppe <joexy79@gmail.com>
2026-06-16 21:24:00 +0200

    fix: correggi DTS per aMatch + aggiungi logs per debuggare annulla

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260616_2000';
+const CACHE_VERSION = '20260616_2124';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
