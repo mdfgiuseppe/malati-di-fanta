7c1fd5f032bde873ad8cba60f3974cb0652a7fde
mdfgiuseppe <joexy79@gmail.com>
2026-06-16 20:00:00 +0200

    fix: aggiungi parentesi di chiusura mancanti

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260615_2138';
+const CACHE_VERSION = '20260616_2000';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
