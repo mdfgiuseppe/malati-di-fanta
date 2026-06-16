fb1a836c5d61243bf4f01f4e89e041ca21eb5b0b
mdfgiuseppe <joexy79@gmail.com>
2026-06-16 22:09:52 +0200

    fix: gestisci Finale con rh1/rh2 legacy e mostra vincitore

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260616_2208';
+const CACHE_VERSION = '20260616_2209';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
