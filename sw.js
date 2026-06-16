64ce0730fbc37d00a61fb038da2a6eb2f84bdee9
mdfgiuseppe <joexy79@gmail.com>
2026-06-16 22:08:13 +0200

    fix: salva Finale come partita singola (rh/ra) non andata/ritorno

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260616_2207';
+const CACHE_VERSION = '20260616_2208';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
