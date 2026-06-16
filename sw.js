be2351e46429d15113583207180db6297c5a9091
mdfgiuseppe <joexy79@gmail.com>
2026-06-16 22:07:02 +0200

    fix: mostra totale gol nella Finale (rh1+rh2 vs ra1+ra2)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260616_2205';
+const CACHE_VERSION = '20260616_2207';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
