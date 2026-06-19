8528ec5e5622b2aef6525764c8208bf5675bbe50
mdfgiuseppe <joexy79@gmail.com>
2026-06-19 17:02:21 +0200

    feat: chiedi dts/dcr se Finale è pareggio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260616_2209';
+const CACHE_VERSION = '20260619_1702';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
