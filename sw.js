e0e3ddb579d709c199766b861c918d82ee7e2dd1
mdfgiuseppe <joexy79@gmail.com>
2026-06-19 17:18:22 +0200

    fix: salva risultato dts/dcr anche in rh/ra per la Finale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260619_1708';
+const CACHE_VERSION = '20260619_1718';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
