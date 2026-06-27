80bab9edc7301a4bf0a8380908e5d84ab72adca1
mdfgiuseppe <joexy79@gmail.com>
2026-06-27 08:09:29 +0200

    fix: ripristina URL Google Apps Script (il nuovo era offline)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260627_0807';
+const CACHE_VERSION = '20260627_0809';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
