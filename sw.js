00eae4233f8f11ed7f6d8ebe26a61cf692d8dc17
mdfgiuseppe <joexy79@gmail.com>
2026-07-04 17:08:38 +0200

    fix: aumenta delay JSONP da 5ms a 100ms per evitare race condition con callback

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260704_1706';
+const CACHE_VERSION = '20260704_1708';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
