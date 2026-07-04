e30971bd5c3b28e4bfebbd04b1e648cbc98048d2
mdfgiuseppe <joexy79@gmail.com>
2026-07-04 17:26:20 +0200

    fix: aumenta delay JSONP a 300ms e timeout ping a 10s per caricamenti paralleli robusti

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260704_1723';
+const CACHE_VERSION = '20260704_1726';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
