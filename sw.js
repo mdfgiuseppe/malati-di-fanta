a06c3ecf91531093bebee37e2a69295374c1d4b6
mdfgiuseppe <joexy79@gmail.com>
2026-07-04 18:41:40 +0200

    fix: consolida tutta la renderizzazione dentro renderClassifica() per evitare race conditions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260704_1837';
+const CACHE_VERSION = '20260704_1841';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
