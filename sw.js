c235cc0b244b08c10946dad19418937a727c4d3d
mdfgiuseppe <joexy79@gmail.com>
2026-07-05 15:20:40 +0200

    debug: aggiungi log nel flusso openQualif() per tracciare caricamento override

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260705_1509';
+const CACHE_VERSION = '20260705_1520';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
