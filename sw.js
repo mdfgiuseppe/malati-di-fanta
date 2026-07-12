83e200c8fc51160e0a88b313aa4afcca0fa5f49d
mdfgiuseppe <joexy79@gmail.com>
2026-07-12 09:36:45 +0200

    refactor: usa Google Sheet per tutti i dati, carica tutto al boot

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260707_1919';
+const CACHE_VERSION = '20260712_0936';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
