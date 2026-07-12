1b93635fb5dbdab681dac569e23fb670464292cc
mdfgiuseppe <joexy79@gmail.com>
2026-07-12 09:44:02 +0200

    refactor: rimuovi sync localStorage, usa solo gSheetData in memoria

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260712_0943';
+const CACHE_VERSION = '20260712_0944';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
