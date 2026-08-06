ae4829bbddcde49f3a01b7dc4475b403f986f827
mdfgiuseppe <joexy79@gmail.com>
2026-08-06 09:31:10 +0200

    fix: riduce delay JSONP a 700ms per evitare problemi sincronizzazione

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260806_0910';
+const CACHE_VERSION = '20260806_0931';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
