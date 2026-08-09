99e27ca721040f7d542a3e41442c204ab16c166a
mdfgiuseppe <joexy79@gmail.com>
2026-08-09 17:14:33 +0200

    fix: attiva pulsante Risultati - consenti visualizzazione calendario anche senza dati

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260806_0934';
+const CACHE_VERSION = '20260809_1714';
 const CACHE_NAME = 'mdf-' + CACHE_VERSION;
 
 self.addEventListener('install', () => self.skipWaiting());
